a8e4d97622247d705116ebc619a2f21c36607739
Roman <kuprov@gmail.com>
2025-06-07 09:46:17 -0400

    combine migrations

--- migrations/00001_add_user_table.sql
+++ migrations/00001_add_user_table.sql
@@ -1,48 +1,61 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE users (
-    id UUID PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    password_hash VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE users
+(
+    id            UUID PRIMARY KEY,
+    name          VARCHAR(100)        NOT NULL,
+    email         VARCHAR(100) UNIQUE NOT NULL,
+    password_hash VARCHAR(255)        NOT NULL,
+    created_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-CREATE INDEX index_email ON users(email);
+CREATE INDEX index_email ON users (email);
 
-CREATE TABLE homes (
-    id UUID PRIMARY KEY,
-    owner_id UUID NOT NULL,
-    address VARCHAR(255) NOT NULL,
-    city VARCHAR(100) NOT NULL,
-    state VARCHAR(100) NOT NULL,
-    zip_code VARCHAR(20) NOT NULL,
-    description TEXT,
-    tags TEXT[],
-    image VARCHAR(255),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (owner_id) REFERENCES users(id)
+CREATE TABLE homes
+(
+    id               UUID PRIMARY KEY,
+    owner_id         UUID         NOT NULL,
+    name             VARCHAR(100) NOT NULL,
+    street_address_1 VARCHAR(255) NOT NULL,
+    street_address_2 VARCHAR(255),
+    city             VARCHAR(100) NOT NULL,
+    state            VARCHAR(100) NOT NULL,
+    zip_code         VARCHAR(20)  NOT NULL,
+    country          VARCHAR(100) NOT NULL,
+    description      TEXT,
+    tags             TEXT[],
+    image            VARCHAR(255),
+    created_at       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (owner_id) REFERENCES users (id)
 );
-CREATE INDEX index_owner_id ON homes(owner_id);
-CREATE INDEX index_state ON homes(state);
-CREATE INDEX index_city ON homes(city);
+CREATE INDEX index_owner_id ON homes (owner_id);
+CREATE INDEX index_state ON homes (state);
+CREATE INDEX index_city ON homes (city);
 CREATE INDEX index_tags ON homes USING GIN (tags);
 
-CREATE TABLE sessions (
-    id UUID PRIMARY KEY,
-    user_id UUID NOT NULL,
+CREATE TABLE sessions
+(
+    id            UUID PRIMARY KEY,
+    user_id       UUID                NOT NULL,
     session_token VARCHAR(255) UNIQUE NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    created_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
+CREATE TABLE user_sessions
+(
+    session_token TEXT PRIMARY KEY,
+    user_id       UUID        NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    expires_at    TIMESTAMPTZ NOT NULL
+);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
 DROP TABLE homes;
 DROP TABLE sessions;
+DROP TABLE IF EXISTS user_sessions;
 DROP TABLE users;
 -- +goose StatementEnd
